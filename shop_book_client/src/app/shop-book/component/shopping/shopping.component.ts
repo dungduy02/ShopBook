b612665859e757ae002f387930c9b6925b478477
NguyenKhanh2104 <181300110@st.hcmuaf.edu.vn>
2022-05-14 23:51:18 +0700

    display all book in shoppingPage

--- shop_book_client/src/app/shop-book/component/shopping/shopping.component.ts
+++ shop_book_client/src/app/shop-book/component/shopping/shopping.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from '../../service/book.service';
 
 @Component({
   selector: 'app-shopping',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping.component.css']
 })
 export class ShoppingComponent implements OnInit {
-
-  constructor() { }
+  listBooks : any[]=[];
+  constructor(private bookService:BookService) { }
 
   ngOnInit(): void {
+    this.bookService.getBooks().subscribe
+    (data=>{
+      this.listBooks=data;
+    })
+    
   }
 
 }
